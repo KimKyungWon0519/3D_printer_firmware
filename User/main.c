668e7fa205961013f8f42927798d7a00e616f30a
KimKyungWon <gwon080870@gmail.com>
2021-06-24 13:29:02 +0900

    [ADD] add y move using while

--- User/main.c
+++ User/main.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <stdarg.h>
 
+#define ONE_MM 80
+
 #define X_STEP (0x01 << 9)
+#define Y_STEP (0x01 << 11)
 
 volatile unsigned long sys_count = 0;
 volatile unsigned int TimingDelay = 0;
@@ -12,8 +15,10 @@ void init();
 void systick_init();
 void rcc_init();
 void x_motor_init();
+void y_motor_init();
 void timer2_init();
 void nvic_init();
+void move_x(double distance);
 void Delay(unsigned int nTime);
 void TimingDelay_Decrement(void);
 void TIM2_IRQHandler (void);
@@ -21,6 +26,7 @@ void TIM2_IRQHandler (void);
 char toggle_x = 0;
 char move_x_enable = 0;
 unsigned int step_count_x = 0;
+unsigned int max_count_x = 0;
 
 int main(void)
 {	
@@ -28,16 +34,25 @@ int main(void)
 
 	init();
 	
-	move_x_enable = 1;
-	step_count_x = 0;
+	while(1) {
+		GPIOE->ODR |= Y_STEP;
+		Delay(20);
+		GPIOE->ODR &= ~Y_STEP;
+		Delay(20);
+	}
+}
 
-	while(1);
+void move_x(double distance) {
+	max_count_x = distance * ONE_MM;
+	step_count_x = 0;
+	move_x_enable = 1;
 }
 
 void init() {
 	systick_init();
 	rcc_init();
 	x_motor_init();
+	y_motor_init();
 	timer2_init();
 	nvic_init();
 }
@@ -49,6 +64,7 @@ void systick_init() {
 }
 
 void rcc_init() {
+	RCC->AHB1ENR |= (0x01 << 3);	// GPIOD
 	RCC->AHB1ENR |= (0x01 << 4);	// GPIOE
 	RCC->AHB1ENR |= (0x01 << 5);	// GPIOF
 	RCC->APB1ENR |= (0x01 << 0);	// TIM2
@@ -64,6 +80,18 @@ void x_motor_init() {
 	GPIOE->MODER |= (0x01 << ( 9 * 2 ));		// ouput mode
 	GPIOE->OTYPER |= (0x00 << 9);						// push pull
 	GPIOE->OSPEEDR |= (0x03 << (9 * 2));	// Very high speed 
+}
+
+void y_motor_init() {	
+	/* y enable ( PD7 ) */
+	GPIOD->MODER |= (0x01 << 7 * 2);			// ouput mode
+	GPIOD->OTYPER |= (0x00 << 7);					// push pull
+	GPIOD->OSPEEDR |= (0x03 << 7 * 2);	// Very high speed 
+
+	/* y step ( PE11 ) */
+	GPIOE->MODER |= (0x01 << ( 11 * 2 ));		// ouput mode
+	GPIOE->OTYPER |= (0x00 << 11);						// push pull
+	GPIOE->OSPEEDR |= (0x03 << (11 * 2));	// Very high speed 
 }
 
 void timer2_init() {
@@ -82,14 +110,14 @@ void TIM2_IRQHandler (void) {
 	if ((TIM2->SR & 0x0001) != 0) { 
 		if(move_x_enable) {
 			if(toggle_x == 0) {
-				GPIOE->ODR |= (0x01 << 9);
+				GPIOE->ODR |= X_STEP;
 				toggle_x = 1;
 			} else {
-				GPIOE->ODR &= ~(0x01 << 9);
+				GPIOE->ODR &= ~X_STEP;
 				toggle_x = 0;
 				
 				if(step_count_x >= 4000) {
-					RCC->APB1ENR &= ~(0x01 << 0);
+					move_x_enable = 0;
 				}
 				
 				step_count_x++;
