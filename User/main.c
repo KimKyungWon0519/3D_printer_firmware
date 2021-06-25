9edc95539f2cc10a83072fa9e8e8d5023cbb05f0
KimKyungWon <gwon080870@gmail.com>
2021-06-25 20:47:07 +0900

    [ADD] gotoXY

--- User/main.c
+++ User/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdarg.h>
 
-#define ONE_MM 80
+#define ONE_CM 800
 
 #define X_STEP (0x01 << 9)
 #define Y_STEP (0x01 << 11)
@@ -17,8 +17,11 @@ void rcc_init();
 void x_motor_init();
 void y_motor_init();
 void timer2_init();
+void timer3_init();
 void nvic_init();
 void move_x(double distance);
+void move_y(double distance);
+void gotoXY(double x, double y);
 void Delay(unsigned int nTime);
 void TimingDelay_Decrement(void);
 void TIM2_IRQHandler (void);
@@ -27,6 +30,10 @@ char toggle_x = 0;
 char move_x_enable = 0;
 unsigned int step_count_x = 0;
 unsigned int max_count_x = 0;
+char toggle_y = 0;
+char move_y_enable = 0;
+unsigned int step_count_y = 0;
+unsigned int max_count_y = 0;
 
 int main(void)
 {	
@@ -34,18 +41,26 @@ int main(void)
 
 	init();
 	
-	while(1) {
-		GPIOE->ODR |= Y_STEP;
-		Delay(20);
-		GPIOE->ODR &= ~Y_STEP;
-		Delay(20);
-	}
+	gotoXY(5, 0);
+	
+	while(1);
+}
+
+void gotoXY(double x, double y) {
+	move_x(x);
+	move_y(y);
 }
 
 void move_x(double distance) {
-	max_count_x = distance * ONE_MM;
+	max_count_x = distance * ONE_CM;
 	step_count_x = 0;
 	move_x_enable = 1;
+}
+
+void move_y(double distance) {
+	max_count_y = distance * ONE_CM;
+	step_count_y = 0;
+	move_y_enable = 1;
 }
 
 void init() {
@@ -54,6 +69,7 @@ void init() {
 	x_motor_init();
 	y_motor_init();
 	timer2_init();
+	timer3_init();
 	nvic_init();
 }
 
@@ -67,7 +83,9 @@ void rcc_init() {
 	RCC->AHB1ENR |= (0x01 << 3);	// GPIOD
 	RCC->AHB1ENR |= (0x01 << 4);	// GPIOE
 	RCC->AHB1ENR |= (0x01 << 5);	// GPIOF
+	
 	RCC->APB1ENR |= (0x01 << 0);	// TIM2
+	RCC->APB1ENR |= (0x01 << 1);	// TIM3
 }
 
 void x_motor_init() {
@@ -102,8 +120,17 @@ void timer2_init() {
 	TIM2->ARR = 200 - 1;					// 200 count
 }
 
+void timer3_init() {
+	TIM3->CR1 |= (0x01 << 0);	// CEN (Counter Enable)
+	TIM3->DIER |= (0x01 << 0);	// UIE ( Update Interrupt Enable )
+	TIM3->CNT = 0;									// Timer Count
+	TIM3->PSC = 84 - 1;						// 1us
+	TIM3->ARR = 200 - 1;					// 200 count
+}
+
 void nvic_init() {
 	NVIC->ISER[0] |= (0x01 << 28);	// TIM2 Enable
+	NVIC->ISER[0] |= (0x01 << 29);	// TIM3 Enable
 }
 
 void TIM2_IRQHandler (void) {
@@ -116,7 +143,7 @@ void TIM2_IRQHandler (void) {
 				GPIOE->ODR &= ~X_STEP;
 				toggle_x = 0;
 				
-				if(step_count_x >= 4000) {
+				if(step_count_x >= max_count_x) {
 					move_x_enable = 0;
 				}
 				
@@ -125,6 +152,28 @@ void TIM2_IRQHandler (void) {
 		}
 
 	TIM2->SR &= ~(1<<0); // clear UIF flag
+	}		
+}
+
+void TIM3_IRQHandler (void) {
+	if ((TIM3->SR & 0x0001) != 0) { 
+		if(move_y_enable) {
+			if(toggle_y == 0) {
+				GPIOE->ODR |= Y_STEP;
+				toggle_y = 1;
+			} else {
+				GPIOE->ODR &= ~Y_STEP;
+				toggle_y = 0;
+				
+				if(step_count_y >= max_count_y) {
+					move_y_enable = 0;
+				}
+				
+				step_count_y++;
+			}
+		}
+
+	TIM3->SR &= ~(1<<0); // clear UIF flag
 	}		
 }
 
